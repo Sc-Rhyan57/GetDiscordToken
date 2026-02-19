6ffcb4bb46468aa1b68ed83dd42c314bfaa71894
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-18 23:45:52 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    id("com.android.application")        version "8.2.0"  apply false
-    kotlin("android")                    version "1.9.10" apply false
-    kotlin("jvm")                        version "1.9.10" apply false
-    id("org.jetbrains.compose")          version "1.6.11" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.10" apply false
+    id("com.android.application") version "8.2.0"  apply false
+    kotlin("android")             version "1.9.10" apply false
+    kotlin("jvm")                 version "1.9.10" apply false
+    id("org.jetbrains.compose")   version "1.6.11" apply false
 }
