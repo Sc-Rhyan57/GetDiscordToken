38206afabdb89c7e2dbcda920de8dec4ee918cc3
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-18 23:21:39 -0300

    Add Kotlin JVM and Compose plugins to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    kotlin("android") version "1.9.10" apply false
+    id("com.android.application")        version "8.2.0"  apply false
+    kotlin("android")                    version "1.9.10" apply false
+    kotlin("jvm")                        version "1.9.10" apply false
+    id("org.jetbrains.compose")          version "1.6.11" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.10" apply false
 }
